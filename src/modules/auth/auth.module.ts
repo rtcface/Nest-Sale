896474ff5b83576440e5231becf4930820e8b665
Ramiro Tepehua <rtcface@gmail.com>
2021-12-15 16:33:32 -0600

    module oaut delete resolver

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
-import { AuthResolver } from './auth.resolver';
+
 import { JwtModule } from '@nestjs/jwt';
 import { JWT_SECRET_TOKEN } from '../../server.constants';
 import { JwtStrategy } from './strategies/jwt.strategy';
@@ -21,6 +21,6 @@ import { AuthController } from './auth.controller';
     }),
   ],
   controllers: [ AuthController ],
-  providers: [AuthService, LocalStrategy, AuthResolver, JwtStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
